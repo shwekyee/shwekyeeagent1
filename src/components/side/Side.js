f8e1ccc94792fc54d4a721b6c5897ed8876b0030
Shwe Kyee <shwekyee.football@gmail.com>
2022-07-13 11:15:55 +0630

    kaungsan

--- src/components/side/Side.js
+++ src/components/side/Side.js
@@ -3,7 +3,7 @@ import './side.css'
 import { Image } from 'react-bootstrap'
 import { ChatDotsFill, TelephoneFill } from 'react-bootstrap-icons'
 
-import profile from '../../Image/minndinn.jpg';
+import profile from '../../Image/kaungsan.jpg';
 
 const Side = () => {
   return (
@@ -12,7 +12,7 @@ const Side = () => {
             <Image fluid src={profile}></Image>
         </div>
         {/* <a href='https://a3678.live4000.com/Chat/Chat?userID=&lang=en' className='bg-light text-dark'><ChatDotsFill></ChatDotsFill> Customer Support</a> */}
-        <a href='https://t.me/minndinn' className='side-viber'><TelephoneFill></TelephoneFill> Telegram</a>
+        <a href='https://t.me/kaungsan000' className='side-viber'><TelephoneFill></TelephoneFill> Telegram</a>
     </div>
   )
 }
